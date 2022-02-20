6d2ca373ed043f55969a6f7d41adfcc7a8e5461d
jaylearnjs <jaylearnjs@gmail.com>
2022-02-20 19:54:44 +0530

    Esc Keypress Event

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ function openModal() {
 }
 
 function closeModal() {
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModal);
 }
 
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click',closeModal)
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
